5ba5af252a1b3d9885dfb338621a471534f181a9
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-03-30 20:42:41 +0530

    merge sort

--- REVISION/sorting/sorting.cpp
+++ REVISION/sorting/sorting.cpp
@@ -16,3 +16,35 @@ void insertionSort(vector<int>& arr) {
         }
     }
 } 
+
+
+
+// merge sort
+  void merge(vector<int>& arr , int mid){
+      vector<int>v1;
+      vector<int>v2;
+  
+      for(int i = 0 ; i<=mid;i++)v1.push_back(arr[i]);
+      for(int i = mid+1 ; i<arr.size() ; i++)v2.push_back(arr[i]);
+  
+      int i =0 , j=0 , k =0;
+      while(i<v1.size() && j<v2.size()){
+          if(v1[i] < v2[j]) arr[k++] = v1[i++];
+          else arr[k++] = v2[j++];
+      }
+  
+      while(i<v1.size()) arr[k++] = v1[i++];
+      while(j<v2.size()) arr[k++] = v2[j++];
+      
+  }
+  
+  void mergeSort(vector<int>& arr , int l , int r){
+      if(l>=r) return;
+  
+      int mid = (l+r)/2;
+  
+      mergeSort(arr , l , mid);
+      mergeSort(arr , mid+1 , r);
+  
+      merge(arr , mid);
+  }
