61ebac34478f9376214171fb666c3c31a19933cd
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-02 10:30:57 +0530

    // LC 1752 - check if the array is sorted and rotated

--- REVISION/sorting/sorting.cpp
+++ REVISION/sorting/sorting.cpp
@@ -87,24 +87,3 @@ void quickSort(vector<int> &arr, int start, int end) {
 
 
 
-// LC 1752 - check if the array is sorted and rotated 
-class Solution {
-public:
-bool check(vector<int>& nums) {
-    int count = 0;
-    int n = nums.size();
-
-    for(int i=1; i<n; i++) {  // when the array both sorted and rotated
-        if(nums[i-1] > nums[i]) {
-            count++;
-        }
-    }
-
-    if(nums[n-1] > nums[0]) { // when the array is sorted and not rotated
-        count++;
-    }
-
-    return count <= 1;
-}
-
-};
