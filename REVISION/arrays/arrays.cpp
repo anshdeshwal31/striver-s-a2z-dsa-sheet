bcf610fb63f417e0e88db707db5f193b142f806e
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-02 11:39:20 +0530

    / LC 26 - remove duplicates from sorted array

--- REVISION/arrays/arrays.cpp
+++ REVISION/arrays/arrays.cpp
@@ -3,23 +3,37 @@ using namespace std;
 
 
 // LC 1752 - check if the array is sorted and rotated 
-class Solution {
-    public:
-    bool check(vector<int>& nums) {
-        int count = 0;
-        int n = nums.size();
-    
-        for(int i=1; i<n; i++) {  // when the array both sorted and rotated
-            if(nums[i-1] > nums[i]) {
-                count++;
-            }
-        }
-    
-        if(nums[n-1] > nums[0]) { // when the array is sorted and not rotated
+bool check(vector<int>& nums) {
+    int count = 0;
+    int n = nums.size();
+
+    for(int i=1; i<n; i++) {  // when the array both sorted and rotated
+        if(nums[i-1] > nums[i]) {
             count++;
         }
+    }
+
+    if(nums[n-1] > nums[0]) { // when the array is sorted and not rotated
+        count++;
+    }
+
+    return count <= 1;
+}
+
+
+// LC 26 - remove duplicates from sorted array 
+int removeDuplicates(vector<int>& nums) {
+    int n = nums.size();
     
-        return count <= 1;
+    if (n <= 1) return n;
+    
+    int j = 1; 
+    
+    for(int i = 1; i < n; i++) {
+        if(nums[i] != nums[i-1]) {
+            nums[j++] = nums[i]; 
+        }
     }
     
-    };
+    return j; 
+}
