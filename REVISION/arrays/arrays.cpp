ae1f45dd6c67168335f9e5f787b2a92b0dffd622
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-04 19:58:48 +0530

    longest subarray with sum k - gfg practice

--- REVISION/arrays/arrays.cpp
+++ REVISION/arrays/arrays.cpp
@@ -77,3 +77,38 @@ int maxSubArray(vector<int>& nums) {
     }
     return max(sum , maxSum);
 }
+
+
+
+// longest subarray with sum k 
+// https://practice.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=longest-sub-array-with-sum-k
+
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size();
+
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        int rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
