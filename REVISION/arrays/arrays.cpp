634eabf0fa1ff78407d95830668f933c0f55b351
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-05 12:56:56 +0530

    LC 31- next permutation

--- REVISION/arrays/arrays.cpp
+++ REVISION/arrays/arrays.cpp
@@ -162,3 +162,31 @@ int longestConsecutive(vector<int>& nums) {
     }
     return maxStreak;
 }
+
+
+// LC 31 - next permutation 
+void nextPermutation(vector<int>& A) {
+    int ind = -1;
+    int n = A.size();
+    
+    for(int i = n - 2; i>=0; i--) {
+        if(A[i] < A[i+1]) {
+            ind = i;
+            break;
+        }
+    }
+    
+    if(ind == -1) {
+        reverse(A.begin(), A.end());
+        return ;
+    }
+    
+    for(int i = n-1; i>ind; i--) {
+        if(A[i] > A[ind]) {
+            swap(A[i], A[ind]);
+            break;
+        }
+    }
+    
+    reverse(A.begin() + ind + 1, A.end());
+}
