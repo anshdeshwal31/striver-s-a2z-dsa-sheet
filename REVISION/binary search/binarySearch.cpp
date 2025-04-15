38058b9019a08931693b5851fa6daba3ce65d49a
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-15 20:03:39 +0530

    LC-33. Search in Rotated Sorted Array

--- REVISION/binary search/binarySearch.cpp
+++ REVISION/binary search/binarySearch.cpp
@@ -98,3 +98,34 @@ vector<int> searchRange(vector<int>& nums, int target) {
     int last = lastOccurence(nums, nums.size(), target);
     return {first, last};   
 }
+
+
+
+// LC - 33. Search in Rotated Sorted Array
+int search(std::vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
