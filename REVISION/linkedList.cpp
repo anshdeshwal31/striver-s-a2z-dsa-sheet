494adc4e55550332d8b05bdcbf23ebc2dfd69d3c
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-19 18:03:28 +0530

    LC 445 - add two numbers 2

--- REVISION/linkedList.cpp
+++ REVISION/linkedList.cpp
@@ -62,3 +62,46 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     delete dummy;
     return result;
 }
+
+
+// LC 445 - add two numbers 2
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    stack<int> s1, s2;
+
+    while (l1) {
+        s1.push(l1->val);
+        l1 = l1->next;
+    }
+
+    while (l2) {
+        s2.push(l2->val);
+        l2 = l2->next;
+    }
+
+    int carry = 0;
+    ListNode* result = nullptr;
+
+    while (!s1.empty() || !s2.empty() || carry) {
+        int sum = carry;
+
+        if (!s1.empty()) {
+            sum += s1.top();
+            s1.pop();
+        }
+
+        if (!s2.empty()) {
+            sum += s2.top();
+            s2.pop();
+        }
+
+        carry = sum / 10;
+
+        // Insert new node at the front of result list
+        ListNode* newNode = new ListNode(sum % 10);
+        newNode->next = result;
+        result = newNode;
+    }
+
+    return result;
+}
