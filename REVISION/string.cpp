9225b23c676dcc1cf6cffb99e326e167e0786160
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-19 12:43:27 +0530

    LC 5 - longest palindromic substring

--- REVISION/string.cpp
+++ REVISION/string.cpp
@@ -24,3 +24,37 @@ bool rotateString(string s, string goal) {
 bool rotateString(string s, string goal) {
     return s.size() == goal.size() && (s + s).find(goal);
 }
+
+
+
+// LC 5 - longest palindromic substring
+string longestPalindrome(string s) {
+    int n = s.length();
+    if (n == 0) return "";
+
+    int start = 0, maxLen = 1;
+
+    for (int center = 0; center < n; ++center) {
+        // Odd length palindrome
+        int l = center, r = center;
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            if (r - l + 1 > maxLen) {
+                start = l;
+                maxLen = r - l + 1;
+            }
+            l--; r++;
+        }
+
+        // Even length palindrome
+        l = center, r = center + 1;
+        while (l >= 0 && r < n && s[l] == s[r]) {
+            if (r - l + 1 > maxLen) {
+                start = l;
+                maxLen = r - l + 1;
+            }
+            l--; r++;
+        }
+    }
+
+    return s.substr(start, maxLen);
+}
