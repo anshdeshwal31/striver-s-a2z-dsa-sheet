47469278cf78b6895f7f2bd4829b3937f72d90f4
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-20 02:32:57 +0530

    LC 22 - generate parenthesis

--- REVISION/recursion.cpp
+++ REVISION/recursion.cpp
@@ -67,3 +67,27 @@ int myAtoi(string s) {
 
     return sign * result;
 }
+
+
+
+// LC 22 - generate parenthesis
+void backtrack(int open, int close, int n, string current, vector<string>& result) {
+    if (current.length() == 2 * n) {
+        result.push_back(current);
+        return;
+    }
+
+    if (open < n) {
+        backtrack(open + 1, close, n, current + '(', result);
+    }
+
+    if (close < open) {
+        backtrack(open, close + 1, n, current + ')', result);
+    }
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> result;
+    backtrack(0, 0, n, "", result);
+    return result;
+}
