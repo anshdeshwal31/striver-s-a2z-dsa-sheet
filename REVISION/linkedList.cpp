eec4fc3769904d38ceb6ec383506522d8a180ee6
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-19 17:23:24 +0530

    LC 2 - add two numbers

--- REVISION/linkedList.cpp
+++ REVISION/linkedList.cpp
@@ -31,3 +31,34 @@ ListNode* oddEvenList(ListNode* head) {
 
     return head;
 }
+
+
+
+// LC 2- add two numbers
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* current = dummy;
+    int carry = 0;
+    
+    while (l1 || l2 || carry) {
+        int sum = carry;
+        
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        
+        carry = sum / 10;
+        current->next = new ListNode(sum % 10);
+        current = current->next;
+    }
+    
+    ListNode* result = dummy->next;
+    delete dummy;
+    return result;
+}
