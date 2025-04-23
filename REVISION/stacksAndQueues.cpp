7eb32431d3ce57fda4fd90e60ade7cdfad0266f8
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-23 19:27:19 +0530

    queue implementation using array

--- REVISION/stacksAndQueues.cpp
+++ REVISION/stacksAndQueues.cpp
@@ -39,3 +39,38 @@ class Stack {
         }
         
 };
+
+
+
+// queue implementation using array - https://www.naukri.com/code360/problems/queue-using-array-or-singly-linked-list_2099908?leftPanelTabValue=PROBLEM
+class Queue {
+    public:
+        int fFront ;
+        int rear;
+        vector<int>queueVector;
+
+        Queue() {
+            this->fFront = 0;
+            this->rear = 0;
+            queueVector = vector<int>(10000 , 0);
+        }
+    
+        bool isEmpty() {
+            return fFront == rear;
+        }
+    
+        void enqueue(int data) {
+            queueVector[rear++] = data; 
+        }
+    
+        int dequeue() {
+            if(fFront == rear) return -1;
+            return queueVector[fFront ++ ];
+        }
+        
+        int front() {
+            if(fFront == rear) return -1;
+            return queueVector[fFront];
+        }
+
+    };
