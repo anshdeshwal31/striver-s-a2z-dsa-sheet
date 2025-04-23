ed976f51e8347466a74203805f446f56fcd39a5c
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-24 00:16:15 +0530

    LC 20 - valid parenthesis

--- REVISION/stacksAndQueues.cpp
+++ REVISION/stacksAndQueues.cpp
@@ -146,5 +146,36 @@ class MyStack {
             delete temp;
             return popped;
         }
-    };
+};
     
+
+
+// LC 20 - valid parenthesis
+bool isValid(string s) {
+    stack<char> st;
+    
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] == '{' || s[i] == '(' || s[i] == '[') {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')') {
+            if(st.empty() || st.top() != '(') {
+                return false;
+            }
+            st.pop();
+        }
+        else if (s[i] == '}') {
+            if(st.empty() || st.top() != '{') {
+                return false;
+            }
+            st.pop();
+        }
+        else if (s[i] == ']') {
+            if(st.empty() || st.top() != '[') {
+                return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+}
