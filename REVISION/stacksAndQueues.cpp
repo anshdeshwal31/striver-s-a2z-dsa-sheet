32ca0aaaec6e4a7d73f48c10723c4ec30317f471
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-24 12:39:02 +0530

    LC 155 - Min Stack

--- REVISION/stacksAndQueues.cpp
+++ REVISION/stacksAndQueues.cpp
@@ -179,3 +179,69 @@ bool isValid(string s) {
     }
     return st.empty();
 }
+
+
+// LC 155 - Min Stack 
+
+// method 1 
+class MinStack {
+    stack < pair < int, int >> st;
+  
+    public:
+      void push(int x) {
+        int min;
+        if (st.empty()) {
+          min = x;
+        } else {
+          min = std::min(st.top().second, x);
+        }
+        st.push({x,min});
+      }
+  
+    void pop() {
+      st.pop();
+    }
+  
+    int top() {
+      return st.top().first;
+    }
+  
+    int getMin() {
+      return st.top().second;
+    }
+  };
+
+
+//   method 2 
+class MinStack {
+    private:
+        stack<int> st;
+        stack<int> minSt;
+    
+    public:
+        MinStack() {
+        }
+    
+        void push(int val) {
+            st.push(val);
+            if (minSt.empty() || val <= minSt.top()) {
+                minSt.push(val);
+            }
+        }
+    
+        void pop() {
+            if (st.top() == minSt.top()) {
+                minSt.pop();
+            }
+            st.pop();
+        }
+    
+        int top() {
+            return st.top();
+        }
+    
+        int getMin() {
+            return minSt.top();
+        }
+    };
+    
