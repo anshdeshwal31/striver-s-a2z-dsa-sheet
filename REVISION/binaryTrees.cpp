c2897d632c2bc4acf2280ebe8b1d8d5db2a524be
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-24 20:50:32 +0530

    LC 124 - binary tree maximum path sum

--- REVISION/binaryTrees.cpp
+++ REVISION/binaryTrees.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 class TreeNode{
     public:
+    int val;
     TreeNode* left;
     TreeNode* right;
 
-    TreeNode(){
+    TreeNode(int val){
         this->left  = nullptr;
         this->right = nullptr;
+        this->val = val;
     }
 };
 
@@ -28,4 +30,24 @@ pair<int , int> solve(TreeNode* root){
 int diameterOfBinaryTree(TreeNode* root) {
     pair<int, int> ans = solve(root);
     return ans.first - 1; // subtracted 1 because diameter is the number of edges not the number of nodes
+}
+
+
+
+// LC 124 - binary tree maximum path sum
+int solve(TreeNode* root, int& maxSum) {
+    if (root == NULL) return 0;
+    
+    int leftSum = max(0, solve(root->left, maxSum));
+    int rightSum = max(0, solve(root->right, maxSum));
+    
+    maxSum = max(maxSum, leftSum + rightSum + root->val);
+    
+    return max(leftSum, rightSum) + root->val;
+}
+
+int maxPathSum(TreeNode* root) {
+    int maxSum = INT_MIN;
+    solve(root, maxSum);
+    return maxSum;
 }
