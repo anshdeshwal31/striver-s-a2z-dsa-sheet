0ebf61f6b27955d895e713db8212004cc786c4b3
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-04-28 22:01:59 +0530

    number of distinct islands

--- REVISION/graphs.cpp
+++ REVISION/graphs.cpp
@@ -166,3 +166,40 @@ int orangesRotting(vector<vector<int>>& grid) {
     
         return result;
     }
+
+
+    
+    
+    // number of distinct islands - https://www.geeksforgeeks.org/problems/number-of-distinct-islands/1
+    void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<bool>>& visited, string& path, string dir) {
+        int n = grid.size(), m = grid[0].size();
+        if (i < 0 || j < 0 || i >= n || j >= m || visited[i][j] || grid[i][j] == 0) return;
+
+        visited[i][j] = true;
+        path += dir;
+
+        dfs(i+1, j, grid, visited, path, "D"); // down
+        dfs(i-1, j, grid, visited, path, "U"); // up
+        dfs(i, j+1, grid, visited, path, "R"); // right
+        dfs(i, j-1, grid, visited, path, "L"); // left
+
+        path += "B"; // backtrack
+    }
+
+    int countDistinctIslands(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        set<string> uniqueIslands;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (!visited[i][j] && grid[i][j] == 1) {
+                    string path = "";
+                    dfs(i, j, grid, visited, path, ""); 
+                    uniqueIslands.insert(path);
+                }
+            }
+        }
+
+        return uniqueIslands.size();
+    }
