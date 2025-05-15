db23e1d99b6d98001433d18fd48089ab32a3adce
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-05-15 14:44:21 +0530

    LC 126 - word ladder 2

--- REVISION/graphs.cpp
+++ REVISION/graphs.cpp
@@ -420,3 +420,56 @@ int orangesRotting(vector<vector<int>>& grid) {
 
         return 0;
     }
+
+
+    
+    // LC 126 - word ladder 2
+    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string> &wordList){
+
+        unordered_set<string> st(wordList.begin(), wordList.end()); 
+        queue<vector<string>> q;
+        q.push({beginWord});
+
+        // A vector defined to store the words being currently used
+        // on a level during BFS.
+        vector<string> usedOnLevel;
+        usedOnLevel.push_back(beginWord);
+        int level = 0;
+        vector<vector<string>> ans;
+
+        while (!q.empty()){
+            vector<string> vec = q.front();
+            q.pop();
+
+            // Now, erase all words that have been
+            // used in the previous levels to transform
+            if (vec.size() > level){
+                level++;
+                for (auto it : usedOnLevel) st.erase(it);
+            }
+
+            string word = vec.back();
+
+            // store the answers if the end word matches with targetWord.
+            if (word == endWord){
+                // the first sequence where we reached end
+                if (ans.size() == 0) ans.push_back(vec);
+                else if (ans[0].size() == vec.size()) ans.push_back(vec);
+            }
+            for (int i = 0; i < word.size(); i++){   
+                char original = word[i];
+                for (char c = 'a'; c <= 'z'; c++){
+                    word[i] = c;
+                    if (st.count(word) > 0){ 
+                        vec.push_back(word);
+                        q.push(vec);
+                        // mark as visited on the level
+                        usedOnLevel.push_back(word);
+                        vec.pop_back();
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return ans;
+    }
