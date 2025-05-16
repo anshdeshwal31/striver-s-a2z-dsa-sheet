8b549fbb1948be813f2c7907e5a703b001b0ad64
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-05-16 21:05:53 +0530

    floyd warshall algo

--- REVISION/graphs.cpp
+++ REVISION/graphs.cpp
@@ -580,3 +580,28 @@ int orangesRotting(vector<vector<int>>& grid) {
         if(negativeCycle) return {-1};
         return dist;
     }
+
+    
+
+
+    // floyd warshall algo 
+    void floydWarshall(vector<vector<int>> &dist) {
+        // Code here
+        int n = dist.size();
+        
+        for(int  i= 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i==j)dist[i][j] = 0;
+            }
+        }
+        
+        for(int via = 0 ; via < n ; via++){
+            for(int i = 0 ; i<n;i++){
+                for(int j =0 ; j < n ; j++){
+                    if(dist[i][via]!=1e8 && dist[via][j]!=1e8)
+                        dist[i][j] = min(dist[i][j], dist[i][via]+dist[via][j]);
+                }
+            }
+        }
+        
+    }
