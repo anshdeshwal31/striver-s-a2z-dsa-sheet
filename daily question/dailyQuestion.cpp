3d7a800566aaf030327a75255ff878d6a962010b
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-09 20:48:50 +0530

    LC-386

--- daily question/dailyQuestion.cpp
+++ daily question/dailyQuestion.cpp
@@ -24,3 +24,25 @@ vector<int> lexicalOrder(int n) {
     dfs(0,n,res);
     return res;
 }
+
+// using iteration
+
+vector<int> lexicalOrder(int n) {
+    vector<int> res;
+    int currNum = 1;
+
+    for (int i = 0; i < n; i++) {
+        res.push_back(currNum);
+
+        if (currNum * 10 <= n) {
+            currNum *= 10;
+        } else {
+            while (currNum % 10 == 9 || currNum >= n) {
+                currNum /= 10;
+            }
+            currNum += 1;
+        }
+    }
+
+    return res;
+}
