5058c05d16ea27d0e8d6c4d336df179179cafbed
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-10 03:58:36 +0530

    minimum platforms

--- greedy/greedy.cpp
+++ greedy/greedy.cpp
@@ -146,3 +146,28 @@ int activitySelection(vector<int>& start, vector<int>& finish) {
 
     return ans;
 }
+
+
+// minimum platforms - http://geeksforgeeks.org/problems/minimum-platforms-1587115620/1
+
+int findPlatform(vector<int>& arr, vector<int>& dep) {
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    sort(dep.begin(), dep.end());
+
+    int plat_needed = 1, max_platforms = 1;
+    int i = 1, j = 0;
+
+    while (i < n && j < n) {
+        if (arr[i] <= dep[j]) {
+            plat_needed++;
+            i++;
+        } else {
+            plat_needed--;
+            j++;
+        }
+        max_platforms = max(max_platforms, plat_needed);
+    }
+
+    return max_platforms;
+}
