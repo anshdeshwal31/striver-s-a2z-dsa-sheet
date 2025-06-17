fd76de45063140e2d41f80300f78541df0fbad32
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-17 23:09:16 +0530

    LC 167

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -380,3 +380,27 @@ vector<vector<int>> generate(int numRows) {
     solve(numRows,ans);
     return ans;
 }
+
+
+
+// LC - 167. Two Sum II - Input Array Is Sorted
+
+vector<int> twoSum(vector<int>& numbers, int target) {
+    int l = 0;
+    int r = numbers.size() - 1;
+
+    while (l < r) {
+        int curSum = numbers[l] + numbers[r];
+        if (curSum > target) {
+            r--;
+        }
+        else if (curSum < target) {
+            l++;
+        }
+        else { // found the match
+            return {l + 1, r + 1}; // 1-based indexing
+        }
+    }
+    return {};
+
+}
