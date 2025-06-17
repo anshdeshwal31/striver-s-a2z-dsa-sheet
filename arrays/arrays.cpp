eae58798716317cddc6ac0b54a6c2732ba5aecc2
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-18 01:55:54 +0530

    LC 18

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -437,3 +437,43 @@ vector<vector<int>> threeSum(vector<int>& nums) {
     }
     return result;
 }
+
+
+
+// LC 18 - 4sum
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    vector<vector<int>> res;
+    if (n < 4) return res;
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 3; ++i) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;  // skip duplicate i
+
+        for (int j = i + 1; j < n - 2; ++j) {
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;  // skip duplicate j
+
+            long long remain = (long long)target - nums[i] - nums[j];
+            int left = j + 1, right = n - 1;
+
+            while (left < right) {
+                long long sum2 = (long long)nums[left] + nums[right];
+                if (sum2 < remain) {
+                    ++left;
+                } else if (sum2 > remain) {
+                    --right;
+                } else {
+                    res.push_back({nums[i], nums[j], nums[left], nums[right]});
+                    ++left; --right;
+
+                    while (left < right && nums[left] == nums[left - 1]) ++left;    // skip duplicate left
+                    while (left < right && nums[right] == nums[right + 1]) --right; // skip duplicate right
+                }
+            }
+        }
+    }
+
+    return res;
+}
