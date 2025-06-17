42f9c7ada33f9434e4fc09f4579b220b1440fd8c
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-18 00:14:01 +0530

    LC 15

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -404,3 +404,36 @@ vector<int> twoSum(vector<int>& numbers, int target) {
     return {};
 
 }
+
+
+
+// LC 15 - 3sum
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end()); // Sort first
+    vector<vector<int>> result;
+
+    for(int i = 0; i < nums.size(); i++) {
+        if (i > 0 && nums[i] == nums[i-1]) continue; // Skip duplicates
+
+        int left = i + 1;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum == 0) {
+                result.push_back({nums[i], nums[left], nums[right]});
+
+                left++;
+                right--;
+                
+                // Skip duplicates
+                while (left < right && nums[left] == nums[left-1]) left++;
+                while (left < right && nums[right] == nums[right+1]) right--;
+            }
+            else if (sum < 0) left++;
+            else right--;
+        }
+    }
+    return result;
+}
