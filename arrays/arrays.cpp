7f0d196b450eb5e8f696e3c4995833d6d0d38c56
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-23 01:25:06 +0530

    LC 493

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -633,3 +633,78 @@ int mergeSort(vector<int> &arr, int low, int high) {
         // Your Code Here
         return mergeSort(arr,0,arr.size()-1);
     }
+
+
+// LC 493 - reverse pairs
+
+int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //Modification 1: cnt variable to count the pairs:
+    int cnt = 0;
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= 2*arr[right]+1) {
+            left++;
+        }
+        else {
+            cnt += (mid - left + 1); //Modification 2
+            right++;
+        }
+    }
+
+    left = low;
+    right = mid+1;
+    while(left <= mid && right<=high){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+
+    return cnt; // Modification 3
+}
+
+
+int mergeSort(vector<int>& nums , int left , int right ){
+    if(left>=right) return 0;
+
+    int count = 0;
+    int mid = left+(right-left)/2;
+
+    count+=mergeSort(nums, left,mid);
+    count += mergeSort(nums , mid+1,right);
+    count += merge(nums , left , mid , right);
+
+    return count;
+}
+
+int reversePairs(vector<int>& nums) {
+    return mergeSort(nums ,0,nums.size()-1);
+}
