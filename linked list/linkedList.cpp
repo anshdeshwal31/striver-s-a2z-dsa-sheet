049a94d69dab9abf8a64c56c79506658c4f1a3b5
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-23 20:32:47 +0530

    flattening a linked list

--- linked list/linkedList.cpp
+++ linked list/linkedList.cpp
@@ -522,3 +522,67 @@ public:
         return newHead;
     }
 };
+
+
+
+// flattening a linked list -https://www.geeksforgeeks.org/problems/flattening-a-linked-list/1
+
+class Solution {
+  public:
+    // Function which returns the  root of the flattened linked list.
+    // Node *flatten(Node *root) {
+    //     // Your code here
+        
+    // }
+    Node* merge(Node* list1, Node* list2){
+    // Create a dummy node as a
+    // placeholder for the result
+    Node* dummyNode = new Node(-1);
+    Node* res = dummyNode;
+
+    // Merge the lists based on data values
+    while(list1 != NULL && list2 != NULL){
+        if(list1->data < list2->data){
+            res->bottom = list1;
+            res = list1;
+            list1 = list1->bottom;
+        }
+        else{
+            res->bottom = list2;
+            res = list2;
+            list2 = list2->bottom;
+        }
+        res->next = NULL;
+    }
+
+    // Connect the remaining
+    // elements if any
+    if(list1){
+        res->bottom = list1;
+    } else {
+        res->bottom = list2;
+    }
+
+    // Break the last node's
+    // link to prevent cycles
+    if(dummyNode->bottom){
+        dummyNode->bottom->next = NULL;
+    }
+
+    return dummyNode->bottom;
+}
+
+// Flattens a linked list with child pointers
+Node* flatten(Node* head){
+    // If head is null or there 
+    // is no next node, return head
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+
+    // Recursively flatten the
+    // rest of the linked list
+    Node* mergedHead = flatten(head->next);
+    head = merge(head, mergedHead);
+    return head;
+}
