b3099c5404461fd9b1720b5ab037e676338534af
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-25 01:57:22 +0530

    the painter's partition  problem  2(painter partition's problem)

--- binarySearch/binarySearch.cpp
+++ binarySearch/binarySearch.cpp
@@ -641,3 +641,76 @@ int splitArray(vector<int> &a, int k) {
     }
     return low;
 }
+
+
+
+// the painter's  partition 2 - https://www.geeksforgeeks.org/problems/the-painters-partition-problem1535/1
+  
+//   brute force
+  int countPainters(vector<int> &boards, int time) {
+    int n = boards.size(); //size of array.
+    int painters = 1;
+    long long boardsPainter = 0;
+    for (int i = 0; i < n; i++) {
+        if (boardsPainter + boards[i] <= time) {
+            //allocate board to current painter
+            boardsPainter += boards[i];
+        }
+        else {
+            //allocate board to next painter
+            painters++;
+            boardsPainter = boards[i];
+        }
+    }
+    return painters;
+}
+
+int minTime(vector<int> &boards, int k) {
+    int low = *max_element(boards.begin(), boards.end());
+    int high = accumulate(boards.begin(), boards.end(), 0);
+
+    for (int time = low; time <= high; time++) {
+        if (countPainters(boards, time) <= k) {
+            return time;
+        }
+    }
+    return low;
+}
+
+
+// using binary search
+
+int countPainters(vector<int> &boards, int time) {
+    int n = boards.size(); //size of array.
+    int painters = 1;
+    long long boardsPainter = 0;
+    for (int i = 0; i < n; i++) {
+        if (boardsPainter + boards[i] <= time) {
+            //allocate board to current painter
+            boardsPainter += boards[i];
+        }
+        else {
+            //allocate board to next painter
+            painters++;
+            boardsPainter = boards[i];
+        }
+    }
+    return painters;
+}
+
+int minTime(vector<int> &boards, int k) {
+    int low = *max_element(boards.begin(), boards.end());
+    int high = accumulate(boards.begin(), boards.end(), 0);
+    //Apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int painters = countPainters(boards, mid);
+        if (painters > k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
