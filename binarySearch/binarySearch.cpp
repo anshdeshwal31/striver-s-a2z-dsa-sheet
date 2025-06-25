f5b0dd58be918a22bff0f5da3f131383bc310313
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-25 19:36:55 +0530

    LC  74

--- binarySearch/binarySearch.cpp
+++ binarySearch/binarySearch.cpp
@@ -718,3 +718,23 @@ int minTime(vector<int> &boards, int k) {
 
 
 // LC 2643 - row with maximum ones
+
+
+
+// LC 74 - search a 2D matrix
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    //apply binary search:
+    int low = 0, high = n * m - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int row = mid / m, col = mid % m;
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
