9021cbfbf239c8d161664821dbae61e85aecbd65
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-07-02 18:50:32 +0530

    LC 84

--- stacks and queues/stacksAndQueues.cpp
+++ stacks and queues/stacksAndQueues.cpp
@@ -324,3 +324,41 @@ string removeKdigits(string num, int k) {
     // If result is empty, return "0"
     return result.empty() ? "0" : result;
 }
+
+
+
+// LC 84 - largest rectangle in histogram
+
+int largestRectangleArea(vector<int>& heights) {
+    int n = heights.size();
+    vector<int> nextSmaller(n);
+    vector<int> prevSmaller(n);
+
+    stack<int>st;
+    for(int i = n-1; i >=0; i--){
+        while(!st.empty() && heights[st.top()]>=heights[i])st.pop();
+
+        if(st.empty()) nextSmaller[i] = n;
+        else nextSmaller[i] = st.top();
+
+        st.push(i);
+    }
+    
+    while(!st.empty())st.pop();
+
+    for(int i = 0; i<n; i++){
+        while(!st.empty() && heights[st.top()]>=heights[i])st.pop();
+
+        if(st.empty()) prevSmaller[i] = -1;
+        else prevSmaller[i] = st.top();
+
+        st.push(i);
+    }
+
+    int ans = 0;
+    for(int i = 0 ; i <n; i++){
+        int area = heights[i]*(nextSmaller[i] - prevSmaller[i]-1);
+        ans = max(ans , area);
+    }
+    return ans;
+}
