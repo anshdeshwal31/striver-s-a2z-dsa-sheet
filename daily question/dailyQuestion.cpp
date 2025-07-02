7c506830da43afaf40568a2d9c4a1ba6436baada
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-07-03 01:04:15 +0530

    LC 3330

--- daily question/dailyQuestion.cpp
+++ daily question/dailyQuestion.cpp
@@ -96,3 +96,17 @@ int findKthNumber(int n, int k) {
 
     return currNum;
 }
+
+
+
+// LC - 3330. Find the Original Typed String I
+
+int possibleStringCount(string word) {
+    int n = word.size();
+
+    int count=0;
+    for(int i= 1 ; i < n ; i ++){
+        if(word[i]==word[i-1])count ++;
+    }
+    return count + 1;
+}
