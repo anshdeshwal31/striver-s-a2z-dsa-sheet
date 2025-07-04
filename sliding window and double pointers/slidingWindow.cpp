50c3866f0d7a1fc40dfb7d7dc2471b692ddd964e
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-07-04 20:51:06 +0530

    LC 992

--- sliding window and double pointers/slidingWindow.cpp
+++ sliding window and double pointers/slidingWindow.cpp
@@ -396,3 +396,29 @@ int kDistinctChars(int k, string &str){
     return ans;
 }
 
+
+
+// LC - 992. Subarrays with K Different Integers
+
+    int atMostKDistinct(vector<int>& nums, int k) {
+        int left = 0, ans = 0;
+        unordered_map<int, int> mp;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            mp[nums[right]]++;
+
+            while (mp.size() > k) {
+                mp[nums[left]]--;
+                if (mp[nums[left]] == 0) mp.erase(nums[left]);
+                left++;
+            }
+
+            ans += (right - left + 1);
+        }
+
+        return ans;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMostKDistinct(nums, k) - atMostKDistinct(nums, k - 1);
+    }
