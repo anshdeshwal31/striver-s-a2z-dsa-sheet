298aeb5ed485fc22695d75884336f8287ed8ef33
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-07-08 01:04:29 +0530

    LC 295

--- heaps/heap.cpp
+++ heaps/heap.cpp
@@ -1,160 +1,184 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-//  codestudio : build min heap 
-void heapify(vector<int>& arr, int n, int i) {
+//  codestudio : build min heap
+void heapify(vector<int> &arr, int n, int i)
+{
     int leftChildIndex = 2 * i + 1;
     int rightChildIndex = 2 * i + 2;
     int largest = i;
 
     // Check if left child exists and is greater than the root
-    if (leftChildIndex < n && arr[leftChildIndex] > arr[largest]) {
+    if (leftChildIndex < n && arr[leftChildIndex] > arr[largest])
+    {
         largest = leftChildIndex;
     }
 
     // Check if right child exists and is greater than the largest so far
-    if (rightChildIndex < n && arr[rightChildIndex] > arr[largest]) {
+    if (rightChildIndex < n && arr[rightChildIndex] > arr[largest])
+    {
         largest = rightChildIndex;
     }
 
     // If the largest is not the root, swap and continue heapifying
-    if (largest != i) {
+    if (largest != i)
+    {
         swap(arr[i], arr[largest]);
         heapify(arr, n, largest);
     }
 }
 
-vector<int> buildHeap(vector<int> arr, int n){
-    for (int i= n/2-1; i>=0;i--){
-        heapify(arr, n , i);
+vector<int> buildHeap(vector<int> arr, int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
     }
     return arr;
 }
 
-
-
 // check if the array represents max heap
-bool solve(int arr[] , int n , int i ){
-    if(i>=n/2) return true;
+bool solve(int arr[], int n, int i)
+{
+    if (i >= n / 2)
+        return true;
 
     int leftChildIndex = 2 * i + 1;
     int rightChildIndex = 2 * i + 2;
 
-    if(rightChildIndex<n){
-        return arr[i]>arr[leftChildIndex] && arr[i]>arr[rightChildIndex];
+    if (rightChildIndex < n)
+    {
+        return arr[i] > arr[leftChildIndex] && arr[i] > arr[rightChildIndex];
     }
-    else{
-        if(leftChildIndex<n){
-            return arr[i]>arr[leftChildIndex];
+    else
+    {
+        if (leftChildIndex < n)
+        {
+            return arr[i] > arr[leftChildIndex];
         }
-        else{
+        else
+        {
             return true;
         }
     }
 }
-bool isMaxHeap(int arr[], int n){
-    for (int i = n/2-1; i >=0; i--){
-        if (!solve(arr , n , i )){
+bool isMaxHeap(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        if (!solve(arr, n, i))
+        {
             return false;
         }
     }
     return true;
 }
 
-
-
-
 // LC 215
 
-int findKthLargest(vector<int>& nums, int k) {
-    priority_queue<vector<int>,greater<int>> pq;
-    for (int   i = 0;   i< k;i  ++){
+int findKthLargest(vector<int> &nums, int k)
+{
+    priority_queue<vector<int>, greater<int>> pq;
+    for (int i = 0; i < k; i++)
+    {
         pq.push(nums[i]);
     }
 
-    for(int i = k ; i<nums.size();i++){
-        if(nums[i]>pq.top()) {
+    for (int i = k; i < nums.size(); i++)
+    {
+        if (nums[i] > pq.top())
+        {
             pq.push(nums[i]);
-        }  
+        }
     }
 
     return pq.top();
 }
 
-
-
 // LC 23
-struct CompareNodes {
-    bool operator()(ListNode* a, ListNode* b) {
+struct CompareNodes
+{
+    bool operator()(ListNode *a, ListNode *b)
+    {
         return a->val > b->val;
     }
 };
 
-ListNode* mergeKLists(vector<ListNode*>& lists) {
-    int k  = lists.size();
-    priority_queue<ListNode*,vector<ListNode*>,CompareNodes> pq;
-    for (int  i = 0; i < k; i++){
+ListNode *mergeKLists(vector<ListNode *> &lists)
+{
+    int k = lists.size();
+    priority_queue<ListNode *, vector<ListNode *>, CompareNodes> pq;
+    for (int i = 0; i < k; i++)
+    {
         pq.push(lists[i]);
     }
-    
-    ListNode*  ansListHead = new ListNode(-1);
-    ListNode* tempNode = new ListNode();
+
+    ListNode *ansListHead = new ListNode(-1);
+    ListNode *tempNode = new ListNode();
     ansListHead->next = tempNode;
-    while(!pq.empty()){
-        ListNode* temp = pq.top();
+    while (!pq.empty())
+    {
+        ListNode *temp = pq.top();
         int tempVal = temp->val;
 
         tempNode->val = temp->val;
         tempNode->next = new ListNode(tempVal);
         tempNode = tempNode->next;
-        
-        if(temp->next) pq.push(temp->next);
+
+        if (temp->next)
+            pq.push(temp->next);
     }
 
-    ListNode* newHead = ansListHead->next;
+    ListNode *newHead = ansListHead->next;
     ansListHead->next = nullptr;
-    delete  ansListHead;
-    
+    delete ansListHead;
+
     return newHead;
 }
 
-
-
 // LC - 355. Design Twitter
 
-class Twitter {
+class Twitter
+{
     int time;
     unordered_map<int, unordered_set<int>> followees;
     unordered_map<int, vector<pair<int, int>>> tweets;
 
 public:
-    Twitter() {
+    Twitter()
+    {
         time = 0;
     }
 
-    void postTweet(int userId, int tweetId) {
+    void postTweet(int userId, int tweetId)
+    {
         tweets[userId].push_back({time++, tweetId});
     }
 
-    vector<int> getNewsFeed(int userId) {
-        priority_queue<pair<int, int>> pq;  // max-heap on time
+    vector<int> getNewsFeed(int userId)
+    {
+        priority_queue<pair<int, int>> pq; // max-heap on time
 
         // Add user's own tweets
-        for (auto &t : tweets[userId]) {
+        for (auto &t : tweets[userId])
+        {
             pq.push(t);
         }
 
         // Add tweets of followees
-        for (auto &f : followees[userId]) {
-            for (auto &t : tweets[f]) {
+        for (auto &f : followees[userId])
+        {
+            for (auto &t : tweets[f])
+            {
                 pq.push(t);
             }
         }
@@ -162,7 +186,8 @@ public:
         // Get top 10 recent tweets
         vector<int> res;
         int cnt = 0;
-        while (!pq.empty() && cnt < 10) {
+        while (!pq.empty() && cnt < 10)
+        {
             res.push_back(pq.top().second);
             pq.pop();
             cnt++;
@@ -170,157 +195,179 @@ public:
         return res;
     }
 
-    void follow(int followerId, int followeeId) {
-        if (followerId == followeeId) return;
+    void follow(int followerId, int followeeId)
+    {
+        if (followerId == followeeId)
+            return;
         followees[followerId].insert(followeeId);
     }
 
-    void unfollow(int followerId, int followeeId) {
+    void unfollow(int followerId, int followeeId)
+    {
         followees[followerId].erase(followeeId);
     }
 };
 
-
-
 // minimum cost to cut ropes - https://www.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
-
-
 
 // LC - 703. Kth Largest Element in a Stream
 
+int solve(int i, int j, int time, vector<vector<int>> &waitCost, int m, int n, vector<vector<vector<int>>> &dp)
+{
+    if (i == m - 1 && j == n - 1)
+        return m * n;
+    if (i >= m)
+        return INT_MAX;
+    if (j >= n)
+        return INT_MAX;
+    if (dp[i][j][time] != -1)
+        return dp[i][j][time];
 
-
-int solve(int i , int j , int time , vector<vector<int>>& waitCost ,int m , int n, vector<vector<vector<int>>>&dp ){
-    if(i==m-1 && j ==n-1) return m*n;
-    if(i>=m)return INT_MAX;
-    if(j>=n) return INT_MAX;
-    if(dp[i][j][time]!=-1)return dp[i][j][time];
-
-
-    if(time%2==0){
-        return dp[i][j][time] = waitCost[i][j] + solve(i,j,time+1,waitCost ,  m ,  n,dp);
+    if (time % 2 == 0)
+    {
+        return dp[i][j][time] = waitCost[i][j] + solve(i, j, time + 1, waitCost, m, n, dp);
     }
 
-    int moveDown = solve(i+1,j,time+1,waitCost,m,n,dp);
-    int moveRight = solve(i,j+1,time+1,waitCost,m,n,dp);
+    int moveDown = solve(i + 1, j, time + 1, waitCost, m, n, dp);
+    int moveRight = solve(i, j + 1, time + 1, waitCost, m, n, dp);
 
-    return dp[i][j][time] =  min(moveDown,moveRight)+ (i+1)*(j+1);
+    return dp[i][j][time] = min(moveDown, moveRight) + (i + 1) * (j + 1);
 }
 
-    long long minCost(int m, int n, vector<vector<int>>& waitCost) {
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(2*m*n+1,-1)));
-        return solve(0,0,1,waitCost , m,n,dp);
+long long minCost(int m, int n, vector<vector<int>> &waitCost)
+{
+    vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(2 * m * n + 1, -1)));
+    return solve(0, 0, 1, waitCost, m, n, dp);
+}
+
+int solveTab(vector<vector<int>> &waitCost, int m, int n)
+{
+    vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(2 * m * n + 1, -1)));
+
+    for (int i = 0; i < 2 * m * n + 1; i++)
+    {
+        dp[m - 1][n - 1][i] = m * n;
     }
-    
-    
-    
-    int solveTab ( vector<vector<int>>&waitCost , int m , int n){
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(2*m*n+1,-1)));
-        
-        for(int i = 0 ; i < 2*m*n+1; i++){
-            dp[m-1][n-1][i] = m*n;
-        }
-        
-        for(int i = m-1; i >= 0 ; i--){
-            for(int j = n-1 ; j>=0; j--){
-                for(int time = 2*m*n-1 ; time>=0 ; time--){
-                    if(time%2==0){
-                        dp[i][j][time] = waitCost[i][j] + dp[i][j][time+1];
-                    }
-                    
-                    int moveDown = dp[i+1][j][time+1];
-                    int moveRight = dp[i][j+1][time+1];
-                    
-                    dp[i][j][time] =  min(moveDown,moveRight)+ (i+1)*(j+1);
-                }   
+
+    for (int i = m - 1; i >= 0; i--)
+    {
+        for (int j = n - 1; j >= 0; j--)
+        {
+            for (int time = 2 * m * n - 1; time >= 0; time--)
+            {
+                if (time % 2 == 0)
+                {
+                    dp[i][j][time] = waitCost[i][j] + dp[i][j][time + 1];
+                }
+
+                int moveDown = dp[i + 1][j][time + 1];
+                int moveRight = dp[i][j + 1][time + 1];
+
+                dp[i][j][time] = min(moveDown, moveRight) + (i + 1) * (j + 1);
             }
         }
-        return dp[0][0][1];
     }
-    
-    long long minCost(int m, int n, vector<vector<int>>& waitCost) {
-        return solveTab(waitCost ,m ,n);
-    }
+    return dp[0][0][1];
+}
 
+long long minCost(int m, int n, vector<vector<int>> &waitCost)
+{
+    return solveTab(waitCost, m, n);
+}
 
-    int dfs(int i , int time ,vector<int>&visited , unordered_map<int , vector<vector<int>>>&adjList){
-        int mini = INT_MAX;
-        visited[i]=true;
-        for(auto& neigh: adjList[i] ){
-            if(!visited[neigh[0]]){
-                int tempTime;
-                if(time>=neigh[2] && time <=neigh[3]){
-                    tempTime = time+1;
-                }
-                else if(time<neigh[2]){
-                    tempTime = neigh[2]+1;
-                }
-                else{
-                    continue;
-                }
-                int recursiveCall = dfs(neigh[0],tempTime , visited,adjList);
-                mini = min(mini ,recursiveCall);
+int dfs(int i, int time, vector<int> &visited, unordered_map<int, vector<vector<int>>> &adjList)
+{
+    int mini = INT_MAX;
+    visited[i] = true;
+    for (auto &neigh : adjList[i])
+    {
+        if (!visited[neigh[0]])
+        {
+            int tempTime;
+            if (time >= neigh[2] && time <= neigh[3])
+            {
+                tempTime = time + 1;
             }
+            else if (time < neigh[2])
+            {
+                tempTime = neigh[2] + 1;
+            }
+            else
+            {
+                continue;
+            }
+            int recursiveCall = dfs(neigh[0], tempTime, visited, adjList);
+            mini = min(mini, recursiveCall);
         }
-        return mini;
+    }
+    return mini;
+}
+
+int minTime(int n, vector<vector<int>> &edges)
+{
+    unordered_map<int, vector<vector<int>>> adjList;
+
+    for (auto &i : edges)
+    {
+        adjList[i[0]].push_back({i[1], i[2], i[3]}); // neighbouring node , start time , end time
     }
 
-    int minTime(int n, vector<vector<int>>& edges) {
-        unordered_map<int , vector<vector<int>>> adjList;
+    vector<int> visited(n, 0);
+    return dfs(0, 0, visited, adjList);
+}
 
-        for(auto &i: edges){
-            adjList[i[0]].push_back({i[1],i[2],i[3]});  // neighbouring node , start time , end time 
-        }
-
-        vector<int>visited(n,0);
-        return dfs(0,0,visited , adjList);
-    }
-
-
-    #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    int dfs(int i , int time ,vector<int>&visited , unordered_map<int , vector<vector<int>>>&adjList){
+    int dfs(int i, int time, vector<int> &visited, unordered_map<int, vector<vector<int>>> &adjList)
+    {
         int mini = INT_MAX;
-        visited[i]=true;
-        for(auto& neigh: adjList[i] ){
-            if(!visited[neigh[0]]){
+        visited[i] = true;
+        for (auto &neigh : adjList[i])
+        {
+            if (!visited[neigh[0]])
+            {
                 int tempTime;
-                if(time>=neigh[2] && time <=neigh[3]){
-                    tempTime = time+1;
+                if (time >= neigh[2] && time <= neigh[3])
+                {
+                    tempTime = time + 1;
                 }
-                else if(time<neigh[2]){
-                    tempTime = neigh[2]+1;
+                else if (time < neigh[2])
+                {
+                    tempTime = neigh[2] + 1;
                 }
-                else{
+                else
+                {
                     continue;
                 }
-                int recursiveCall = dfs(neigh[0],tempTime , visited,adjList);
-                mini = min(mini ,recursiveCall);
+                int recursiveCall = dfs(neigh[0], tempTime, visited, adjList);
+                mini = min(mini, recursiveCall);
             }
         }
         return mini;
     }
 
-    int minTime(int n, vector<vector<int>>& edges) {
-        unordered_map<int , vector<vector<int>>> adjList;
+    int minTime(int n, vector<vector<int>> &edges)
+    {
+        unordered_map<int, vector<vector<int>>> adjList;
 
-        for(auto &i: edges){
-            adjList[i[0]].push_back({i[1],i[2],i[3]});  // neighbouring node , start time , end time 
+        for (auto &i : edges)
+        {
+            adjList[i[0]].push_back({i[1], i[2], i[3]}); // neighbouring node , start time , end time
             // cout<<" "<< adjList[i[0]][0][0]<< " "<<adjList[i[0]][0][1]<<" " << adjList[i[0]][0][2]<<" ";
         }
-        
-        vector<int>visited(n,0);
-        return dfs(0,0,visited , adjList);
+
+        vector<int> visited(n, 0);
+        return dfs(0, 0, visited, adjList);
     }
 };
 
-
-
 // Maximum Sum Combination- https://www.geeksforgeeks.org/problems/maximum-sum-combination/1
 
-vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+vector<int> topKSumPairs(vector<int> &a, vector<int> &b, int k)
+{
     int n = a.size();
 
     // Sort both arrays in descending order
@@ -339,7 +386,8 @@ vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
 
     vector<int> result;
 
-    while (k-- && !maxHeap.empty()) {
+    while (k-- && !maxHeap.empty())
+    {
         auto top = maxHeap.top();
         maxHeap.pop();
 
@@ -350,13 +398,15 @@ vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
         result.push_back(sum);
 
         // Next pair: (i+1, j)
-        if (i + 1 < n && visited.find({i + 1, j}) == visited.end()) {
+        if (i + 1 < n && visited.find({i + 1, j}) == visited.end())
+        {
             maxHeap.push({a[i + 1] + b[j], {i + 1, j}});
             visited.insert({i + 1, j});
         }
 
         // Next pair: (i, j+1)
-        if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
+        if (j + 1 < n && visited.find({i, j + 1}) == visited.end())
+        {
             maxHeap.push({a[i] + b[j + 1], {i, j + 1}});
             visited.insert({i, j + 1});
         }
@@ -364,3 +414,53 @@ vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
 
     return result;
 }
+
+
+
+// LC - 295. Find Median from Data Stream
+
+class MedianFinder
+{
+public:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    MedianFinder(){}
+
+    void balance()
+    {
+        if (!maxHeap.empty() && maxHeap.size() > minHeap.size() + 1)
+        {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+        else if (!minHeap.empty() && minHeap.size() > maxHeap.size() + 1)
+        {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    void addNum(int num)
+    {
+        if(maxHeap.empty() || maxHeap.top()>num){
+            maxHeap.push(num);
+        }
+        else{
+            minHeap.push(num);
+        }
+        balance();
+    }
+
+    double findMedian()
+    {
+        if (maxHeap.empty() && minHeap.empty()) return 0;
+        double result ;
+        if(maxHeap.size()==minHeap.size()){
+            result = (maxHeap.top()+minHeap.top())/2.0;
+        }
+        else{
+            result = maxHeap.size()>minHeap.size()?maxHeap.top():minHeap.top();
+        }
+        return result;
+    }
+};
