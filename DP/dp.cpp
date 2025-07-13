338ee9f7b5c8a4b340d6b095843dc56065a40bda
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-07-14 00:27:53 +0530

    legth of longest increasing subsequence using binary search

--- DP/dp.cpp
+++ DP/dp.cpp
@@ -3380,3 +3380,29 @@ vector<int> printingLongestIncreasingSubsequence(vector<int>& nums, int n) {
     reverse(lis.begin(), lis.end());
     return lis;
 }
+
+
+
+// length of longest increasing subsequence using binary search - https://takeuforward.org/data-structure/longest-increasing-subsequence-binary-search-dp-43/
+
+int longestIncreasingSubsequence(int arr[], int n) {
+    // Create a temporary vector to store the increasing subsequence
+    vector<int> temp;
+    temp.push_back(arr[0]);
+
+    int len = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > temp.back()) {
+            // If arr[i] is greater than the last element of temp, extend the subsequence
+            temp.push_back(arr[i]);
+            len++;
+        } else {
+            // If arr[i] is not greater, replace the element in temp with arr[i]
+            int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[ind] = arr[i];
+        }
+    }
+
+    return len;
+}
