da945f9dc2ed8e90120a64e0611464085a4450e1
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-07-21 00:28:13 +0530

    matrix chain multiplication

--- DP/dp.cpp
+++ DP/dp.cpp
@@ -3619,7 +3619,7 @@ vector<int> largestDivisibleSubset(vector<int>& arr) {
 
 
     // LC - 673. Number of Longest Increasing Subsequence
-    
+
    int findNumberOfLIS(vector<int>& arr) {
     int n = arr.size();
 
@@ -3651,4 +3651,40 @@ vector<int> largestDivisibleSubset(vector<int>& arr) {
     }
 
     return numberOfLIS;
+}
+
+
+
+// matrix chain multiplication - https://www.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1
+
+int f(vector<int>& arr, int i, int j){
+    
+    // base condition
+    if(i == j)
+        return 0;
+        
+    int mini = INT_MAX;
+    
+    // partioning loop
+    for(int k = i; k<= j-1; k++){
+        
+        int ans = f(arr,i,k) + f(arr, k+1,j) + arr[i-1]*arr[k]*arr[j];
+        
+        mini = min(mini,ans);
+        
+    }
+    
+    return mini;
+}
+
+
+int matrixMultiplication(vector<int>& arr, int N){
+    
+    int i =1;
+    int j = N-1;
+    
+    
+    return f(arr,i,j);
+    
+    
 }
