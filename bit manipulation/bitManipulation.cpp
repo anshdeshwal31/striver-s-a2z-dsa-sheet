b8d134d7b8b0ce5cbe85949c5d211a728001850f
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-07-31 16:36:19 +0530

    LC 231

--- bit manipulation/bitManipulation.cpp
+++ bit manipulation/bitManipulation.cpp
@@ -20,3 +20,12 @@ bool isEven(int n) {
     if(n&1) return false;
     else return true;
 }
+
+
+
+// LC - 231 - power of two 
+
+bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0;
+}
