0467cf0a24af423974576291636d155ba859c1cf
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-08-01 16:06:57 +0530

    swap two numbers

--- bit manipulation/bitManipulation.cpp
+++ bit manipulation/bitManipulation.cpp
@@ -69,3 +69,23 @@ bool isPowerOfTwo(int n) {
             k = k << 1; // Left shift to move to next bit
         }
     }
+
+
+
+    // swap two numbers 
+
+    // method 1 - https://www.geeksforgeeks.org/problems/swap-two-numbers3844/1
+    pair<int, int> get(int a, int b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        return {a, b};
+    }
+    
+    // method 2
+    pair<int, int> get(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        return {a, b};
+    }
