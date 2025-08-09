374afd83c7994050869a9826f53c2c5973ac4805
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-08-09 21:38:23 +0530

    LC 240

--- bit manipulation/bitManipulation.cpp
+++ bit manipulation/bitManipulation.cpp
@@ -266,3 +266,24 @@ bool isPowerOfTwo(int n) {
 
         // cout << endl;
     }
+
+
+
+    // LC 204 - count primes
+
+int countPrimes(int n) {
+    if (n <= 2) return 0; // No primes less than 2
+    
+    vector<bool> isPrime(n, true);
+    isPrime[0] = isPrime[1] = false; // 0 and 1 are not prime
+    
+    for (int i = 2; i * i < n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    
+    return count(isPrime.begin(), isPrime.end(), true);
+}
