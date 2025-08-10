63e727c8be2bfb3cbdb4dfde706dcde86862c645
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-08-10 18:02:38 +0530

    LC 50

--- bit manipulation/bitManipulation.cpp
+++ bit manipulation/bitManipulation.cpp
@@ -323,3 +323,26 @@ vector<vector<int>> primeFactorization(vector<int>& queries) {
     }
     return result;
 }
+
+
+
+
+// LC - 50. Pow(x, n)
+
+double myPow(double x, int n) {
+    long long N = n; // use long long to handle INT_MIN
+    if (N < 0) {
+        x = 1 / x;
+        N = -N;
+    }
+    
+    double result = 1.0;
+    while (N > 0) {
+        if (N % 2 == 1) { // if odd
+            result *= x;
+        }
+        x *= x;
+        N /= 2;
+    }
+    return result;
+}
